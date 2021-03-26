c829f70da3d1662b1bb1c41d026498675914e985
Nick Robison <nicholas.a.robison@omb.eop.gov>
2021-03-26 10:42:30 -0400

    Committed a broken test.
    Stack overflow with our consumer?

---
+++ api-application/src/test/java/gov/usds/vaccineschedule/api/services/FetchServiceTest.java
@@ -0,0 +1,52 @@
+package gov.usds.vaccineschedule.api.services;
+
+import gov.usds.vaccineschedule.api.config.ScheduleSourceConfig;
+import gov.usds.vaccineschedule.api.providers.PatientProvider;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.stream.binder.test.InputDestination;
+import org.springframework.cloud.stream.binder.test.OutputDestination;
+import org.springframework.cloud.stream.binder.test.TestChannelBinderConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.messaging.support.GenericMessage;
+
+import java.util.Locale;
+import java.util.function.Function;
+
+/**
+ * Created by nickrobison on 3/26/21
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import({
+        PatientProvider.class
+})
+@EnableConfigurationProperties({
+        ScheduleSourceConfig.class
+})
+public class FetchServiceTest {
+
+    @Autowired
+    InputDestination input;
+    @Autowired
+    OutputDestination output;
+
+    @Test
+    public void testEmptyConfiguration() {
+        this.input.send(new GenericMessage<>("hello".getBytes()));
+//        assertEquals("HELLO".getBytes(), output.receive().getPayload(), "Should have correct payload");
+    }
+
+    @SpringBootApplication
+    @Import(TestChannelBinderConfiguration.class)
+    public static class SampleConfiguration {
+        @Bean
+        public Function<String, String> uppercase() {
+            return v -> v.toUpperCase(Locale.ROOT);
+        }
+    }
+
+}
