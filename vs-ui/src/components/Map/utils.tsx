99f1a6638e9053faaf61e9f516cb55ab3438d4db
Nick Robison <nicholas.a.robison@omb.eop.gov>
2021-04-14 14:37:32 -0400

    Initial commit of Mapbox integration.

---
+++ vs-ui/src/components/Map/utils.tsx
@@ -0,0 +1,25 @@
+import {useEffect, useState} from "react";
+
+export const usePosition = () => {
+    const [position, setPosition] = useState<GeolocationPosition | null>(null);
+    const [error, setError] = useState<string | null>(null);
+
+    const onChange = (position: GeolocationPosition) => {
+        setPosition(position);
+    }
+
+    const onError = (error: GeolocationPositionError) => {
+        setError(error.message);
+    }
+
+    useEffect(() => {
+        const geo = navigator.geolocation;
+        if (!geo) {
+            setError('Geolocation is not supported');
+            return;
+        }
+        geo.getCurrentPosition(onChange, onError);
+    }, []);
+
+    return {...position, error};
+}
--- vs-ui/src/pages/Finder/Finder.tsx
+++ vs-ui/src/pages/Finder/Finder.tsx
@@ -1,3 +1,20 @@
+import {Grid, GridContainer} from "@trussworks/react-uswds";
+import React from "react";
+import {Map} from "../../components/Map/Map";
+
 export const Finder: React.FC<{}> = () => {
-    return (<h1>I'm the finder page</h1>)
+
+
+    return (
+        <GridContainer>
+            <Grid row>
+                <Grid col={5}>
+                    I'm a list
+                </Grid>
+                <Grid col={7}>
+                    <Map/>
+                </Grid>
+            </Grid>
+        </GridContainer>
+    )
 }
